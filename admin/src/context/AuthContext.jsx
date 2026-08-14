18cf8e65ca893fd0269cabe0f8988e95355a2211
Pratik Kalbhor <kalbhorpratik394@gmail.com>
2026-08-14 17:46:20 +0530

    Update AuthContext.jsx

--- admin/src/context/AuthContext.jsx
+++ admin/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { Children } from 'react';
 import { createContext } from 'react'
 export const authDataContext=createContext();
 function AuthContext({children}) {
-    let serverUrl="http://localhost:8000"
+    let serverUrl="https://prakriti-sparsh-backend.onrender.com"
     let value={
         serverUrl
     }
